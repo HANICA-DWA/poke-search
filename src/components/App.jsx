0c84ab663662d10052c1bc82f6c52e03b1d90ea6
Fritz van Deventer <fritzvd@users.noreply.github.com>
2026-03-06 09:57:14 +0100

    voorbeeld met button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,57 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import FindPokemon from "./FindPokemon";
-import Pokemon from "./Pokemon";
-import eevee from "../eevee.json";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import FindPokemon from './FindPokemon';
+import Pokemon from './Pokemon';
+import eevee from '../eevee.json';
 
-const API_URL = "https://pokeapi.co/api/v2/pokemon";
+const API_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+// function App() {
+//   const [tijd, setTijd] = useState(1000);
+//   const [startInterval, setStartInterval] = useState(false);
+//   const [som, setSom] = useState(1);
+//   const [showButton, setShowButton] = useState(false);
+
+//   const somHandler = () => {
+//     console.log('ik tel bij elkaar op');
+//     setSom(som * 2);
+//   };
+
+//   return (
+//     <>
+//       <h1>Som: {som}</h1>
+//       <input value={tijd} onChange={(e) => setTijd(e.target.value)} />
+//       <button onClick={() => setShowButton(!showButton)}>Laat knop zien</button>
+//       {showButton ? (
+//         <ButtonInterval
+//           startInterval={startInterval}
+//           setStartInterval={setStartInterval}
+//           tijd={tijd}
+//           intervalHandler={somHandler}
+//         />
+//       ) : (
+//         ''
+//       )}
+//     </>
+//   );
+// }
+
+// function ButtonInterval({
+//   startInterval,
+//   setStartInterval,
+//   tijd,
+//   intervalHandler,
+// }) {
+//   useEffect(() => {
+//     const intervalId = setInterval(() => intervalHandler(), tijd);
+//   }, [startInterval]);
+
+//   return (
+//     <button onClick={() => setStartInterval(!startInterval)}>
+//       Start/Stop Interval
+//     </button>
+//   );
+// }
 
 function App() {
   const [query, setQuery] = useState(null);
@@ -19,6 +66,8 @@ function App() {
         });
     }
   }, [query]);
+
+  useEffect(() => {});
 
   return (
     <div className="App">
